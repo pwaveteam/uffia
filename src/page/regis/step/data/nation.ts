eb7c7514da25b47d010bfaf0f76baf4fa3f824fb
kaeun <wevyeun@gmail.com>
2024-05-29 20:34:15 +0900

    UI Improvements

--- src/page/regis/step/data/nation.ts
+++ src/page/regis/step/data/nation.ts
@@ -6,4 +6,14 @@ interface NationInterface {
 export const NationList: string[] = [
   '대한민국',
   'United Kingdom',
+  'Afghanistan',
+  'Belgium',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'France',
+  'Germany',
+  'Hungary',
+  'Iceland',
+  'Japan'
 ]
